c2fdbe52976d287343ecd90a9da350b23734cc33
pr0n1x <pr0n1x@yandex.ru>
2024-11-12 14:07:54 +0700

    added benchmarks

--- benchmarks/bench_test.go
+++ benchmarks/bench_test.go
@@ -5,25 +5,35 @@ import (
 	"testing"
 
 	dec "github.com/pr0n1x/decimal-go"
+	"github.com/pr0n1x/go-liners/assert"
 )
 
-const testPrecision = dec.Quecto
+const testPrecision = dec.Nano
 
-//const testPrecision = dec.Nano
-
-func Benchmark_Decimal_Immutable(b *testing.B) {
+func Benchmark_Immutable_Decimal(b *testing.B) {
 	million := testPrecision.MustParse("1000000")
 	divider := testPrecision.MustParse("33.67")
 
 	b.ResetTimer()
 	b.ReportAllocs()
-
 	for i := 0; i < b.N; i++ {
 		_ = million.Div(divider)
 	}
 }
 
-func Benchmark_Decimal_Mutable(b *testing.B) {
+func Benchmark_Immutable_BigRat(b *testing.B) {
+	divider := &big.Rat{}
+	divider.SetString("33.67")
+	million := &big.Rat{}
+	million.SetString("1000000")
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		(&big.Rat{}).Set(million).Quo(million, divider)
+	}
+}
+
+func Benchmark_Mutable_Decimal(b *testing.B) {
 	millions := make([]dec.Decimal, 1_000_000)
 	for i := range millions {
 		millions[i] = testPrecision.MustParse("1000000")
@@ -31,26 +41,12 @@ func Benchmark_Decimal_Mutable(b *testing.B) {
 
 	divider := testPrecision.MustParse("33.67")
 	b.ResetTimer()
-	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		millions[i].Var().Div(divider)
 	}
 }
 
-func Benchmark_BigRat_Immutable(b *testing.B) {
-	divider := &big.Rat{}
-	divider.SetString("33.67")
-	million := &big.Rat{}
-	million.SetString("1000000")
-
-	b.ResetTimer()
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		(&big.Rat{}).Set(million).Quo(million, divider)
-	}
-}
-
-func Benchmark_BigRat_Mutable(b *testing.B) {
+func Benchmark_Mutable_BigRat(b *testing.B) {
 	million := &big.Rat{}
 	million.SetString("1000000")
 	millions := make([]*big.Rat, 1_000_000)
@@ -61,8 +57,54 @@ func Benchmark_BigRat_Mutable(b *testing.B) {
 	divider.SetString("33.67")
 
 	b.ResetTimer()
-	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		millions[i].Quo(million, divider)
+	}
+}
+
+const testBigFrac = "9183278754354899275983457827.98723867459876125012387684587367745647"
+const testBigFracPrecision = 38
+
+func Benchmark_BigFraction_Div_Decimal(b *testing.B) {
+	rat := dec.MustParse(testBigFrac, testBigFracPrecision)
+	three := dec.FromUInt64(3, testBigFracPrecision)
+	results := make([]dec.Decimal, 1_000_000)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		results = append(results, rat.Div(three))
+	}
+}
+
+func Benchmark_BigFraction_Div_BigRat(b *testing.B) {
+	rat := assert.Ok(new(big.Rat).SetString(testBigFrac))
+	three := new(big.Rat).SetInt64(3)
+	results := make([]*big.Rat, 1_000_000)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		results = append(results, new(big.Rat).Quo(rat, three))
+	}
+}
+
+func Benchmark_BigFraction_Mul_Decimal(b *testing.B) {
+	rat := dec.MustParse(testBigFrac, testBigFracPrecision)
+	three := dec.FromUInt64(3, testBigFracPrecision)
+	results := make([]dec.Decimal, 1_000_000)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		results = append(results, rat.Mul(three))
+	}
+}
+
+func Benchmark_BigFraction_Mul_BigRat(b *testing.B) {
+	rat := assert.Ok(new(big.Rat).SetString(testBigFrac))
+	three := new(big.Rat).SetInt64(3)
+	results := make([]*big.Rat, 1_000_000)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		results = append(results, new(big.Rat).Mul(rat, three))
 	}
 }
